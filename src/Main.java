5175c6386aa4a24afe365e9567697f37f7275eae
karen0119 <karengvargasmontenegro@gmail.com>
2023-07-09 21:16:40 -0700

    The issue has been resolved

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         FeatureSun AdamX=new FeatureSun();
         AdamX.name="poya";
-        AdamX.temp=12344231 celsius;
+        AdamX.temp="12344231 celsius";
         AdamX.size="32942km";
 
     }
